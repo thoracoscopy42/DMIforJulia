1e6c1437360d45286d3add0df45df73dd356e01f
Wiktor Kowalski <wiktorkowalski70@gmail.com>
2026-03-24 04:03:06 +0100

    plots for current dataset

--- zadanie1/data_processing/plotting.jl
+++ zadanie1/data_processing/plotting.jl
@@ -1,13 +1,15 @@
 
 function plot_histogram(df::DataFrame, col; bins=30, title=nothing)
+
     col_in_use = collect(skipmissing(df[!, col]))
     
     fig = Figure(size=(900, 600))
     
-    
+    col = replace(string(col), "_" => " ")
+
     ax = Axis(
         fig[1,1],
-        title = isnothing(title) ? "Histogram $(col)" : title,
+        title = isnothing(title) ? "Histogram: $(col)" : title,
         xlabel = string(col),
         ylabel = "Liczność"
         )
@@ -15,6 +17,33 @@ function plot_histogram(df::DataFrame, col; bins=30, title=nothing)
     hist!(ax, col_in_use, bins=bins)
 
     return fig
+end
+
+function plot_boxplot(df::DataFrame, feature, target; title=nothing)
+    valid_rows = .!ismissing.(df[!, feature]) .& .!ismissing.(df[!, target])
+    
+    feature_vals = df[valid_rows, feature]
+    target_vals  = df[valid_rows, target]
+
+    feature_label = replace(string(feature), "_" => " ")
+    target_label  = replace(string(target),  "_" => " ")
 
 
+    groups = unique(feature_vals)
+    x_positions = [findfirst(==(x), groups) for x in feature_vals]
+
+    fig = Figure(size=(900,600))
+    
+    ax = Axis(
+    fig[1,1],
+    title = isnothing(title) ? "Boxplot: $(feature_label) vs $(target_label)" : title,
+    xlabel = feature_label,
+    ylabel = target_label,
+    xticks = (1:length(groups), string.(groups))
+
+    )
+
+    boxplot!(ax, x_positions, target_vals)
+
+    return fig
 end
--- zadanie1/main.jl
+++ zadanie1/main.jl
@@ -18,4 +18,15 @@ profile, size = profile_dataset(df)
 # wybieramy kolumnę salary_in_usd jako nasz target
 # typeof(df[!,:salary_in_usd]) # <- zwróci nam typ Vector{Int64}
 
-plot_histogram(df, :salary_in_usd)
+fig1 = plot_histogram(df, :salary_in_usd, bins=30)
+save("plots/salary_histogram.svg", fig1)
+
+fig2 = plot_boxplot(df, :experience_level, :salary_in_usd)
+save("plots/experience_vs_salary.svg", fig2)
+
+fig3 = plot_boxplot(df, :remote_ratio, :salary_in_usd)
+save("plots/remote_vs_salary.svg", fig3)
+
+display(fig1)
+display(fig2)
+display(fig3)
