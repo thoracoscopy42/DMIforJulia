4a4a5882f52ae1c7668c3f7a5ed113725e5f1fba
Wiktor Kowalski <wiktorkowalski70@gmail.com>
2026-03-24 03:31:41 +0100

    lil whoopsie

--- zadanie1/data_processing/plotting.jl
+++ zadanie1/data_processing/plotting.jl
@@ -7,13 +7,13 @@ function plot_histogram(df::DataFrame, col; bins=30, title=nothing)
     
     ax = Axis(
         fig[1,1],
-        title = isnothing(title) ? "Histogram $(col_in_use)" : title,
+        title = isnothing(title) ? "Histogram $(col)" : title,
         xlabel = string(col),
         ylabel = "Liczność"
         )
 
     hist!(ax, col_in_use, bins=bins)
-    
+
     return fig
 
 
