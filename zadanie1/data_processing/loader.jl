99c267d5aaa0490c0d1cae3736a897e2723129c2
Wiktor Kowalski <wiktorkowalski70@gmail.com>
2026-03-24 18:56:08 +0100

    new loader for csv to dict

--- zadanie1/data_processing/loader.jl
+++ zadanie1/data_processing/loader.jl
@@ -3,3 +3,15 @@ function load_file(filepath::AbstractString)
     df = CSV.read(filepath, DataFrame)
     return df
 end
+
+function load_dict_from_csv(filepath::AbstractString)
+    
+    df = CSV.read(filepath, DataFrame)
+    
+    dictionary = Dict(
+    row[1] => row[2]
+    for row in eachrow(df)
+    )
+    
+    return dictionary
+end
