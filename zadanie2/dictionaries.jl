9d847ce30bfaa398aa8150b08e04166456181516
Wiktor <wiktorkowalski70@gmail.com>
2026-04-13 02:24:55 +0200

    moved dictionaries, because we are working with the same dataset

---
+++ zadanie2/dictionaries.jl
@@ -0,0 +1,36 @@
+experience = Dict(
+    "EN" => 1,
+    "MI" => 2,
+    "SE" => 3,
+    "EX" => 4
+)
+
+employment = Dict(
+    "FT" => 1,
+    "CT" => 2,
+    "PT" => 3,
+    "FL" => 4
+)
+
+company_size = Dict(
+    "S" => 1,
+    "M" => 2,
+    "L" => 3
+)
+
+location = Dict(
+    "North America" => 1,
+    "Europe"        => 1,
+
+    "Asia"          => 2,
+    "South America" => 2,
+
+    "Oceania"       => 3,
+    "Africa"        => 3,
+)
+
+remote = Dict(
+    0               => "on_site",
+    50              => "hybrid",
+    100             => "remote"
+)
